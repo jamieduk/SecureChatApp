1a386961b6d4be03063b38b727185daa96f35196
Jay <jamied_uk@hotmail.com>
2021-06-22 22:48:02 +0100

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -1,28 +1,44 @@
 #!/bin/bash
-# (c) J~Net 2021
+# (c) J~NET 2021
+# jnet.sytes.net
 #
-# https://jnet.forumotion.com/t1744-secure-chat-bash-app#2702
+# https://jnet.forumotion.com/t1729-jnet-multi-tool-2021#2677
 #
-# ./menu.sh
-#
-GREEN='\033[0;32m'
-printf "${GREEN}\n"
-echo "Welcome To Secure Message App 2021 Menu Pick An Option..."
-FILE=/usr/bin/figlet
-if [ -f "$FILE" ]; then
-    figlet Secure Chat
-else 
-    bash setup.sh
-fi
-
-echo "(rx to recieve a secure message or tx to send a secure message)"
+echo -en "\e[92m "
 echo ""
-path="." # .
-select fname in $path/*.sh; #.sh
-do
-    echo you picked $fname \($REPLY\)
-    ./$fname
-    bash menu.sh
-    break;
-done
-bash menu.sh
+echo "Welcome To J~Net Menu"
+echo ""
+echo "1. Show All Messages"
+echo "2. Recieve Message" 
+echo "3. Send Message"
+echo "4. Calendar"
+echo "5. Recieve File"
+echo "6. Send File"
+echo "7. Setup Remote Server"
+echo "8. View Downloaded File"
+echo "9. Remote Shell"
+echo "Enter your choice: (Anything else to exit)" 
+read n
+case $n in
+1)
+clear
+cat all_messages.txt 
+bash menu.sh ;;
+2) bash recieve.sh ;;
+3) bash send.sh ;;
+4) cal 
+bash menu.sh;;
+5) bash file_rx.sh;;
+# send arg here
+6)  echo "Enter Filename"
+    read myvar
+    bash file_tx.sh "$myvar";;
+7) bash setup.sh;;
+8) bash view_file.sh;;
+9) cd remote && bash menu.sh;;
+esac
+
+
+
+
+
