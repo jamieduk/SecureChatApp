c6b9e8d14a1046a67fb95e9a62a3f6dfe5ac8c90
Jay <jamied_uk@hotmail.com>
2021-06-23 16:44:13 +0100

    Add files via upload

--- update_host.sh
+++ update_host.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Linux Bash File & Messenger App (c) J~Net 2021
+# jnet.sytes.net
+#
+# ./setup.sh
+#
+# https://jnet.forumotion.com/t1729-jnet-multi-tool-2021#2677
+# https://jnet.forumotion.com/t1744-secure-chat-bash-app#2702
+#
 echo "Update Remote Host IP"
 echo "Default is localhost delete localhost then put in IP, press enter."
 defaultip="localhost"
