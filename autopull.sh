6e94eb18544828b98018391d0023dc4917381b5c
Jay <jamied_uk@hotmail.com>
2021-06-26 21:06:24 +0100

    Add files via upload

--- autopull.sh
+++ autopull.sh
@@ -5,21 +5,23 @@ echo "$pwd"
 #
 path=$pwd
 cd $path
+account="jamieduk"
+product="SecureChatApp"
 sudo chown -R $USER $path
 
-if cd ../SecureChatApp; 
+if cd ../$product; 
 then
     #cd ..
     git init .
-    git remote add -t \* -f origin https://github.com/jamieduk/SecureChatApp.git
+    git remote add -t \* -f origin https://github.com/$account/$product.git
     git checkout master
     git pull;
 else 
-    #git clone https://github.com/jamieduk/SecureChatApp.git SecureChatApp;
-    git clone https://github.com/jamieduk/SecureChatApp.git SecureChatApp;
+    #git clone https://github.com/jamieduk/$product.git $product;
+    git clone https://github.com/jamieduk/$product.git $product;
 fi
 
 echo ""
-yes | sudo cp -r $path/SecureChatApp/* $path
-sudo rm -rf $path/SecureChatApp
+yes | sudo cp -r $path/$product/* $path
+sudo rm -rf $path/$product
 
