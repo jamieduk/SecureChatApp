5ba09f222422d87e121abee5b015258ac08be417
Jay <jamied_uk@hotmail.com>
2021-06-26 12:25:10 +0100

    Add files via upload

--- update.sh
+++ update.sh
@@ -7,12 +7,14 @@
 # https://jnet.forumotion.com/t1729-jnet-multi-tool-2021#2677
 # https://jnet.forumotion.com/t1744-secure-chat-bash-app#2702
 #
+account="jamieduk"
+product="SecureChatApp"
 pwd=`pwd`
 echo "Update J~NET Multi Tool 2021"
 sudo mkdir update
 sudo chown -R $USER update
 cd update
-git clone https://github.com/jamieduk/SecureChatApp.git
+git clone https://github.com/$account/$product.git
 cd SecureChatApp
 sudo mv -f --ignore-fail-on-non-empty config old
 sudo rmdir --ignore-fail-on-non-empty old
