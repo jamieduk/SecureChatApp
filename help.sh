3e658bf8ffbc45a22d6183d4f1c131132a00de0c
Jay <jamied_uk@hotmail.com>
2021-06-27 09:18:43 +0100

    Add files via upload

--- help.sh
+++ help.sh
@@ -4,7 +4,7 @@
 # https://jnet.forumotion.com/t1744-secure-chat-bash-app#2702
 #
 echo "Welcome To The Secure Message App 2021 Help Page"
-echo ""
+echo "J~Net"|figlet
 echo "1) Show All Messages"
 echo "2) Recieve Message From Remote Location"
 echo "3) Send Message For Sending Secure Text To Remote Location"
@@ -19,7 +19,8 @@ echo "11) Update Alias"
 echo "12) Repair A Corrupted Message"
 echo "13) Update J~NET Multi Tool 2021"
 echo "14) Help"
-echo ""
+echo "Press Enter For More"
+read Y
 echo "Tip: For Security, The KEY is NEVER stored on the hard drive and will be"
 echo "Requested for Recieving & Sending Secure Messages!"
 echo ""
@@ -31,5 +32,4 @@ echo ""
 echo "Added Admin Features and Update Options."
 echo ""
 read -p "Press Enter To Go Back To The Menu..."
-
-
+bash menu.sh
