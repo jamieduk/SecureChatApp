797f1c43f9fb584ce3918b17eebc46dd98f1b359
Jay <jamied_uk@hotmail.com>
2021-07-02 16:34:56 +0100

    Add files via upload

--- generate_key.sh
+++ generate_key.sh
@@ -9,15 +9,29 @@
 #
 num="64" # Key Lengh!
 lines="1"
+final_num=""
 echo "Linux Bash File & Messenger App (c) J~Net 2021"
 echo ""
+echo ""
+read -p "Enter Desired Lengh [$num]: " input
+#
+if [ -z "$input" ]
+then
+    final_num=$num
+    echo "You Selected $final_num"
+else
+    final_num=$input
+    echo "You Selected $final_num"
+fi
+
+#
 echo "Generate Secure Key"
 echo ""
-echo "Generating Strong $num Character Key"
+echo "Generating Strong final_num Character Key"
 echo ""
 for ((n=0;n<$lines;n++))
 do 
-    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-$num
+    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-$final_num
 done
 #
 echo ""
