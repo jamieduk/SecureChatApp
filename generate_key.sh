d0c46dc8d57cca2ea4293ad515b2b33c59040fb2
Jay <jamied_uk@hotmail.com>
2021-07-02 17:41:39 +0100

    Add files via upload

--- generate_key.sh
+++ generate_key.sh
@@ -36,18 +36,69 @@ echo ""
 #echo "$multi Test"
 for ((n=0;n<$lines;n++))
 do 
-    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output1.txt
-    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output2.txt
+    dd if=/dev/urandom count=10 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-1000 > output1.txt
+    dd if=/dev/urandom count=10 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output2.txt
     dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output3.txt
-    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output4.txt
+    dd if=/dev/urandom count=20 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output4.txt
+    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output5.txt
+    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output6.txt
+    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output7.txt
+    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output8.txt
+    dd if=/dev/urandom count=10 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output9.txt
+    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output10.txt
+    dd if=/dev/urandom count=20 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output11.txt
+    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output12.txt
+    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output13.txt
+    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output14.txt
+    dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$final_num" > output15.txt
     cat output1.txt > final.txt
     cat output2.txt >> final.txt
     cat output3.txt >> final.txt
     cat output4.txt >> final.txt
+    cat output5.txt >> final.txt
+    cat output6.txt >> final.txt
+    cat output7.txt >> final.txt
+    cat output8.txt >> final.txt
+    cat output9.txt >> final.txt
+    cat output10.txt >> final.txt
+    cat output11.txt >> final.txt
+    cat output12.txt >> final.txt
+    cat output13.txt >> final.txt
+    cat output14.txt >> final.txt
+    cat output15.txt >> final.txt
+    cat output3.txt >> final.txt
+    cat output10.txt >> final.txt
+    cat output14.txt >> final.txt
+    cat output1.txt >> final.txt
+    cat output2.txt >> final.txt
+    cat output3.txt >> final.txt
+    cat output4.txt >> final.txt
+    cat output5.txt >> final.txt
+    cat output6.txt >> final.txt
+    cat output7.txt >> final.txt
+    cat output8.txt >> final.txt
+    cat output9.txt >> final.txt
+    cat output10.txt >> final.txt
+    cat output11.txt >> final.txt
+    cat output12.txt >> final.txt
+    cat output13.txt >> final.txt
+    cat output14.txt >> final.txt
+    cat output15.txt >> final.txt
+    cat output3.txt >> final.txt
+    cat output10.txt >> final.txt
+    cat output14.txt >> final.txt
+    cat output1.txt >> final.txt
     clear
 
-    #cat output.txt | cut -c-$final_num
-head -c $final_num final.txt
+# Read in only x amount of chars from a text file into a bash var
+echo "Your Strong Key Is"
+echo ""
+output=$(head -c $final_num final.txt)
+echo "$output"
+echo ""
+
+
+
 done
 #
 echo ""
