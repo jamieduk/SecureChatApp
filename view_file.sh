1d94d94b91a66b6a54590c5f28bf36640ed95473
Jay <jamied_uk@hotmail.com>
2021-07-04 14:43:48 +0100

    Add files via upload

--- view_file.sh
+++ view_file.sh
@@ -9,8 +9,17 @@ echo "Welcome To Open Secure File"
 #read password
 cd downloads
 #unzip file.zip #-e $password
-7za e file.7z #-P$password #  -e
-echo "Files Downloaded: "
+file="file.7z"
+if [[ -f $file ]]
+then
+    7za e $file && rm file.7z #-P$password #  -e
+    #echo "<file> exists on your filesystem."
+else
+    echo "No New Downloads To Decrypt Yet"
+fi
+# Remove file.7z after extraction!
+echo ""
+echo "All Files Downloaded Will Be Listed: "
 echo ""
 ls
 echo "Press Enter To Continue"
