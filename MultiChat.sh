29919b5f87300980c638b2c5bdc8c12efe4c553d
Jay <jamied_uk@hotmail.com>
2021-07-06 00:28:15 +0100

    Add files via upload

--- MultiChat.sh
+++ MultiChat.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+host="jnet.sytes.net" # jnet.sytes.net
 echo "Enter Your Alias To Connect To J~Net Multi-Chat"
 read username
-ssh $username@jnet.sytes.net -p 2022
+ssh $username@$host -p 2022
