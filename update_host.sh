124942c583962ca5f64a086f9516ccfdc366a525
Jay <jamied_uk@hotmail.com>
2021-07-06 15:07:31 +0100

    Add files via upload

--- update_host.sh
+++ update_host.sh
@@ -7,9 +7,10 @@
 # https://jnet.forumotion.com/t1729-jnet-multi-tool-2021#2677
 # https://jnet.forumotion.com/t1744-secure-chat-bash-app#2702
 #
-echo "Update Remote Host IP"
-echo "Default is localhost delete localhost then put in IP, press enter."
 defaultip=`cat config/remote_host.txt`
+echo "Update Remote Host IP (Appends to the list) Used For Multiple Hosts!"
+echo "Currently $defaultip delete $defaultip then put in IP, Press Enter."
+
 if [ "$#" -eq  "0" ]
   then
         read -e -p "Enter Remote IP " -i "$defaultip" ip
@@ -17,9 +18,16 @@ else
     ip=$1
 fi
 #
-echo "$ip" >> config/remote_host.txt
-echo "Add New Remote Remote IP"
-cat config/remote_host.txt
-echo "Press Enter To Go Back To Menu"
-read Y
+
+echo "Happy With $ip?"
+read -r -p "[y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    echo "$ip" >> config/remote_host.txt
+    echo "Add New Remote Remote IP"
+    cat config/remote_host.txt
+else
+    echo "Ok Then..."
+fi
+
 bash menu.sh
