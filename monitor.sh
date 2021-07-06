a9d590727123ceb89cec32b61191db7623a0ebba
Jay <jamied_uk@hotmail.com>
2021-07-06 18:23:59 +0100

    Add files via upload

--- monitor.sh
+++ monitor.sh
@@ -5,7 +5,18 @@
 # https://jnet.forumotion.com/t1729-jnet-multi-tool-2021#2677
 # https://jnet.forumotion.com/t1744-secure-chat-bash-app#2702
 #
-port=776
+#see https://stackoverflow.com/questions/29728151/how-to-listen-for-multiple-tcp-connection-using-nc
+#
+#
+defaultport=776
+if [ -z "$1" ];
+then
+# read -p "Enter Port To Monitor [$default_port]: " port
+    port=$defaultport
+    echo ""
+else 
+    port=$1
+fi
 # output after you start listening to same port.
 watch -n1 "sudo netstat -anp | grep $port" # >> log.txt && cat log.txt
 bash menu.sh
