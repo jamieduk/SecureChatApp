e17587b06954b40618f39dcc0013d099a57ee907
Jay <jamied_uk@hotmail.com>
2021-07-12 22:02:30 +0100

    Add files via upload

--- secure_recieve.sh
+++ secure_recieve.sh
@@ -9,7 +9,7 @@ lport=777
 
 if [ ! -f server.pem ]
 then
-    echo "This will now attempt to help you create a key file, fill out the following!..."
+    echo "This will now attempt to help you create a Certificate key file, fill out the following!..."
     openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -keyout server.pem -out server.crt
 fi
 SSL_crt="server.crt"
